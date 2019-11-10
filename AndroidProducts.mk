711d47991e33087155fae5b94cf5c57e3eb6c74b
Melvin <kartik3163@gmail.com>
2019-11-10 18:30:46 +0530

    sanders: Time for some Queso

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/aosip_sanders.mk
+	$(LOCAL_DIR)/bootleg_sanders.mk
 
 COMMON_LUNCH_CHOICES := \
-	aosip_sanders-userdebug
+	bootleg_sanders-userdebug
