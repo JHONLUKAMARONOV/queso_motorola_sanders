00425325d5fb2cc3cf491fad8ee82ed19745d0b0
Melvin <kartik3163@gmail.com>
2019-11-12 14:01:26 +0530

    sepolicy: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    ref: https://android-review.googlesource.com/c/device/google/taimen/+/970688
    
    Signed-off-by: melvinstark3 <kartik3163@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -239,7 +239,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 #Soong
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
