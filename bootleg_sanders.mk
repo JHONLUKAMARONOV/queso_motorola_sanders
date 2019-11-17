a7c1dec6fc7f6996005a39cabcd6c127781d2240
Melvin <kartik3163@gmail.com>
2019-11-18 00:42:03 +0530

    Drop maintainer prop for now

--- bootleg_sanders.mk
+++ bootleg_sanders.mk
@@ -42,7 +42,6 @@ PRODUCT_MODEL := Moto G (5S) Plus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="sanders" \
-    DEVICE_MAINTAINERS := "Melvin" \
     TARGET_DEVICE="sanders" \
     PRIVATE_BUILD_DESC="sanders-user 8.1.0 OPS28.65-36 9fea release-keys"
 
