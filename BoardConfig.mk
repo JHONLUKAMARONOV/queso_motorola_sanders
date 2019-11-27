3b5d9da9c367b67bd3e52d1e380d28583b4be736
luca020400 <luca.stefani.ge1@gmail.com>
2019-11-27 18:44:32 +0530

    sanders: Move soong namespaces to device.mk
    * This is a product property

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -244,9 +244,6 @@ TARGET_USES_OLD_MNC_FORMAT := true
 #include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
-#Soong
-PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
-
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
 
