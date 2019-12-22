4886b28c2c8a6ffdeb9b2e50fa9c99da8627c475
Matthew Bouyack <matthewbouyack@google.com>
2019-12-22 10:03:20 +0530

    sanders: Disable backpressure propagation in SF
    Because the hardware composer has a total latency of
    up to 30ms (from 'handleMessageInvalidate' until the image appears
    on the screen) backpressure propagation causes us to run at 30Hz.
    
    This change allows us to run smoothly at 60Hz.
    
    Signed-off-by: Melvin <kartik3163@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -131,6 +131,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.gralloc.enable_fb_ubwc=1 \
     vendor.display.disable_skip_validate=1
 
+# Enable backpressure for GL comp
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.disable_gl_backpressure=1
+
 # Enable QC2 bufferqueue block-pool
 PRODUCT_PROPERTY_OVERRIDES += vendor.qc2.use.bqpool=1
 
